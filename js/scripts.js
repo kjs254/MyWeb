f333990b9bcf1771e864bdb77aadb256b5eaf1cd
Jisan <149364697+kjs254@users.noreply.github.com>
2024-06-23 22:55:17 +0900

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+window.onscroll = function() {
+    if (document.documentElement.scrollTop > 300) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+};
+
+scrollToTopBtn.addEventListener('click', function() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+
+        targetElement.scrollIntoView({
+            behavior: 'smooth',
+            
+            });
+        
+    });
+});
+
